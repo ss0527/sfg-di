3a968407c01b1d553b014e30eccfd51de720109d
Suganya Stalin <suganya.stalin@ptpinc.com>
2021-06-28 22:04:21 -0700

    Dependency Injection Assignment

--- src/main/java/guru/springframework/sfgdi/services/PetService.java
+++ src/main/java/guru/springframework/sfgdi/services/PetService.java
@@ -1,8 +1,11 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by jt on 12/28/19.
  */
+
 public interface PetService {
 
     String getPetType();
